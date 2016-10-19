040dbdf20cb09ee79853d6be7b13ab49d3c654f5
Vinicius Gerevini <viniciusgerevini@gmail.com>
2016-10-19 23:17:23 +1100

    Implemented mariadb container and added support to PHP

--- files/default/index.php
+++ files/default/index.php
@@ -8,6 +8,19 @@
     <h1>This is the default page!</h1>
     <?php
       phpinfo();
+
+      // $pdo = new \PDO(
+      //   'mysql:host=mariadb;dbname=test_db',
+      //   'root',
+      //   'test_pass'
+      // );
+      //
+      // $query = $pdo->prepare('show tables');
+      // $query->execute();
+      //
+      // while ($row = $query->fetch(PDO::FETCH_NUM)) {
+      //     echo $row[0];
+      // }
     ?>
   </body>
 </html>
